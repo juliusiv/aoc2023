1267fe3b4aff5ee24146f41da80646bdbf00b23c
Julius Alexander IV <julius.alexander@sofarocean.com>
2023-12-04 22:46:15 -0800

    Day 2 part 2

--- 02/day02.ml
+++ 02/day02.ml
@@ -2,23 +2,30 @@ let num_red_cubes = 12
 let num_green_cubes = 13
 let num_blue_cubes = 14
 
-let part1 lines =
-  let parse_line_into_picks line = line
-    (* remove the game label *)
-    |> String.split_on_char ':'
-    |> Core.List.last_exn
-    |> String.split_on_char ';'
-    |> List.map (String.split_on_char ',')
-    |> List.flatten
-    |> List.map String.trim
+let parse_line_into_picks line =
+  let parse_pick_string_into_tuple pick_string =
+    let split = String.split_on_char ' ' pick_string in
+    let count = Stdlib.int_of_string @@ List.hd split in
+    let color = Core.List.last_exn split in
+    (count, color)
   in
+  line
+  (* remove the game label *)
+  |> String.split_on_char ':'
+  |> Core.List.last_exn
+  |> String.split_on_char ';'
+  |> List.map (String.split_on_char ',')
+  |> List.flatten
+  |> List.map String.trim
+  |> List.map parse_pick_string_into_tuple;;
+
+let part1 lines =
   let is_game_possible picks =
-    let is_pick_possible cube_count_str =
-      let split = String.split_on_char ' ' cube_count_str in
-      match split with
-      | n :: ["red"] ->  Stdlib.int_of_string @@ n <= num_red_cubes
-      | n :: ["blue"] ->  Stdlib.int_of_string @@ n <= num_blue_cubes
-      | n :: ["green"] -> Stdlib.int_of_string @@ n <= num_green_cubes
+    let is_pick_possible pick =
+      match pick with
+      | (count, "red") ->  count <= num_red_cubes
+      | (count, "blue") ->  count <= num_blue_cubes
+      | (count, "green") -> count <= num_green_cubes
       | _ -> true
     in
     List.for_all is_pick_possible picks
@@ -29,13 +36,28 @@ let part1 lines =
   |> List.fold_left (+) 0;;
 
 let part2 lines =
-  List.hd lines;;
+  let color_max picks color =
+    picks
+    |> List.filter_map (fun (n, c) -> if c = color then Some n else None)
+    |> List.fold_left max 0
+  in
+  let game_power picks =
+    let red_max = color_max picks "red" in
+    let blue_max = color_max picks "blue" in
+    let green_max = color_max picks "green" in
+    red_max * blue_max * green_max
+  in
+  lines
+  |> List.map parse_line_into_picks
+  |> List.map game_power
+  |> List.fold_left (+) 0;;
 
 let () =
+  (* let lines = Core.In_channel.read_lines "input-1-test.txt" in *)
   let lines = Core.In_channel.read_lines "input.txt" in
   let part1_soln = part1 lines in
   let part2_soln = part2 lines in
   Printf.printf "Part 1: %i\n" part1_soln;
-  Printf.printf "Part 2: %s\n" part2_soln;;
+  Printf.printf "Part 2: %i\n" part2_soln;;
 
 (* dune exec --display quiet --no-print-directory ./day02 *)
--- README.md
+++ README.md
@@ -7,3 +7,11 @@ $ dune exec --display quiet --no-print-directory day01
 Part 1: 53334
 Part 2: 52834
 ```
+
+## Day 2: Cube Conundrum
+
+```
+$ dune exec --display quiet --no-print-directory day02
+Part 1: 2006
+Part 2: 84911
+```
